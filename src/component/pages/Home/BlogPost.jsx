b22a4d495fc1f0b8af84fbf1b9e626f63807d228
Mahmud sajib <mahmudsajib082@gmail.com>
2023-05-19 03:19:25 +0600

    blog card design

---
+++ src/component/pages/Home/BlogPost.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function BlogPost() {
+  return (
+    <div className='mt-16'>
+    <h1 className='text-4xl font-semibold text-center hover:text-blue-500 cursor-pointer'>Blog Post</h1>
+    <hr className='w-48 border-t-2 mx-auto mb-6 mt-4 border-orange-600 ' />
+    <div className='grid lg:grid-cols-2 gap-4'>
+    <div className="card card-side bg-base-100  border">
+  <figure><img src="https://i.ibb.co/tC2pTS7/ob.png" alt="Movie"/></figure>
+  <div className="card-body flex justify-center items-center">
+  <p className='text-gray-400'> Septemner 2021</p>
+    <h2 className="card-title">Few quips galvanized the mock jury box</h2>
+     
+     
+      <button className=" text-blue-400 ">Read More</button>
+    
+  </div>
+</div>
+    <div className="card card-side bg-base-100  border">
+  <figure><img src="https://i.ibb.co/tC2pTS7/ob.png" alt="Movie"/></figure>
+  <div className="card-body flex justify-center items-center">
+  <p className='text-gray-400'> Septemner 2021</p>
+    <h2 className="card-title">Few quips galvanized the mock jury box</h2>
+     
+     
+      <button className=" text-blue-400 ">Read More</button>
+    
+  </div>
+</div>
+    <div className="card card-side bg-base-100  border">
+  <figure><img src="https://i.ibb.co/tC2pTS7/ob.png" alt="Movie"/></figure>
+  <div className="card-body flex justify-center items-center">
+  <p className='text-gray-400'> Septemner 2021</p>
+    <h2 className="card-title">Few quips galvanized the mock jury box</h2>
+     
+     
+      <button className=" text-blue-400 ">Read More</button>
+    
+  </div>
+</div>
+    <div className="card card-side bg-base-100  border">
+  <figure><img src="https://i.ibb.co/tC2pTS7/ob.png" alt="Movie"/></figure>
+  <div className="card-body flex justify-center items-center">
+  <p className='text-gray-400'> Septemner 2021</p>
+    <h2 className="card-title">Few quips galvanized the mock jury box</h2>
+     
+     
+      <button className=" text-blue-400 ">Read More</button>
+    
+  </div>
+</div>
+ 
+    </div>
+    
+    </div>
+  )
+}
+
+export default BlogPost
--- src/component/pages/Home/Home.jsx
+++ src/component/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import useTitle from '../../../hook/UseTitle'
 import Banner from './Banner' 
 import ImagesGallery from './ImagesGallery'
 import Product from './Product'
+ 
+import BlogPost from './BlogPost'
 
 
 function Home() {
@@ -11,7 +13,11 @@ function Home() {
     <div>
     <Banner />
     <Product />
+     <div className='max-w-7xl mx-auto'>
      <ImagesGallery />
+     <BlogPost />
+     </div>
+     
      
     </div>
   )
