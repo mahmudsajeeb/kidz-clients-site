0e1f7ec7138145ff791a40805384ba6d7f71a245
Mahmud sajib <mahmudsajib082@gmail.com>
2023-05-20 03:10:01 +0600

    add a toy form design and functionality add

--- src/component/pages/AddToy/AddToy.jsx
+++ src/component/pages/AddToy/AddToy.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+// import Select from 'react-select';
+import Swal from 'sweetalert2'   
 import useTitle from '../../../hook/UseTitle'
 import { useForm } from "react-hook-form";
 
 function AddToy() {
   useTitle("Add Toys")
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
+    fetch('http://localhost:1000/alltoys',{
+      method:"POST",
+      headers:{'content-type':'application/json'},
+      body:JSON.stringify(data)
+    })
+    .then(res =>res.json())
+    .then(data =>{console.log(data)
+    
+      if(data.insertedId){
+        Swal.fire({
+          title: 'success',
+          text: 'You are Successfully add toy',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+      }
+    
+    })
   }
 
+  const options = [
+    { value: 'Musical Toy', label: 'Musical Toy' },
+    { value: 'Toy Car', label: 'Toy Car' },
+    { value: 'Video Games Toy', label: 'Video Games Toy' },
+  ];
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className="max-w-lg mx-auto">
@@ -28,7 +54,7 @@ function AddToy() {
 
       <div className="mb-4">
         <label htmlFor="name" className="block mb-2 font-medium">
-          Name
+        Seller Name
         </label>
         <input
           type="text"
@@ -39,13 +65,13 @@ function AddToy() {
       </div>
 
       <div className="mb-4">
-        <label htmlFor="sellerName" className="block mb-2 font-medium">
-          Seller Name
+        <label htmlFor="toyName" className="block mb-2 font-medium">
+          Toy Name
         </label>
         <input
           type="text"
-          id="sellerName"
-          {...register('sellerName')}
+          id="toyName"
+          {...register('toyName')}
           className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
         />
       </div>
@@ -66,12 +92,30 @@ function AddToy() {
         <label htmlFor="subCategory" className="block mb-2 font-medium">
           Sub-category
         </label>
-        <input
-          type="text"
+        <select
           id="subCategory"
           {...register('subCategory')}
           className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
+        >
+          <option value='Musical Toy'>Musical Toy</option>
+          <option value="Toy Car">Toy Car</option>
+          <option value="Video Games Toy">Musical Toy</option>
+          
+          {/* Add more options as needed */}
+        </select>
+
+        {/* <select className="text-input" {...register("subCategory")}>
+              <option value="Engineering">engineering</option>
+              <option value="Editor">Editor</option>
+              <option value="writer">Writer</option>
+              <option value="Developer">Developer</option>
+            </select> */}
+        {/* <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+        isMulti
+      /> */}
       </div>
 
       <div className="mb-4">
